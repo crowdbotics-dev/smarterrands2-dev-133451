8b9be996d72e1defb26d5c895116086334114832
Crowdbotics <engineering@crowdbotics.com>
2024-02-14 19:08:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "smarterrands2-dev-133451",
+  url: "https://smarterrands2-dev-133451.botics.co",
+  api: "https://smarterrands2-dev-133451.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
